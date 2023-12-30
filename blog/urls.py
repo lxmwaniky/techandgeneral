369c0e237bd07d047e3bd0558fef29194b3d53ef
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-12-30 09:25:13 +0300

    Delete unnecessary files and code

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-        path('', views.home, name='tng-home'),
-        path('about/', views.about, name='tng-about'),
-        path('contact/', views.contact, name='tng-contact'),
-        path('blog/', views.blog, name='tng-blog')
-]
