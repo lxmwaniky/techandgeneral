67568565e113bced69d9c5a9bbbe6615eb24f5b4
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-12-31 10:46:37 +0300

    Update blog templates and views

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
+posts = [
+        {
+                'author': 'Alex Mwaniki',
+                'title': 'Blog Post 1',
+                'content': 'This is Blog Post 1',
+                'date_posted': '31-Dec 2023'
+        },
+        {
+                'author': 'Alex Mwaniki',
+                'title': 'Blog Post 2',
+                'content': 'This is Blog Post 2',
+                'date_posted': '31-Dec 2023'
+        }
+]
+
 def index(request):
-        return render(request, 'blog/index.html')
+        context = {
+                'posts': posts
+        }
+        return render(request, 'blog/index.html', context)
 
 def about(request):
-        return render(request, 'blog/about.html')
+        return render(request, 'blog/about.html', { 'title': 'About' })
