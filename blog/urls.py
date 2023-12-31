a6598a07f7b7608f06db82972513c0fbfa0d9f68
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-12-31 11:06:40 +0300

    Update base.html with Bootstrap CSS and fix URL names

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.index, name='blog-name'),
+        path('', views.index, name='blog-home'),
         path('about/', views.about, name='blog-about')
 ]
